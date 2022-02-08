453bea307527ea491a51613b25f76be16fdaeaf2
Narine Manukyan <narine.manukyan@technamin.com>
2022-02-08 22:18:16 +0400

    Initializing Shopping Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Models;
+
+public class ShoppingModel
+{
+    public string UserName { get; set; }
+    public BasketModel BasketModelWithProducts { get; set; }
+    public IEnumerable<OrderResponseModel> Orders { get; set; }
+}
