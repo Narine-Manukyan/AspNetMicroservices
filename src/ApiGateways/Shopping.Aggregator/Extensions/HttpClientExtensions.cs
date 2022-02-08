46ece6c7feca559ea300f5219f51670b4abab2ea
Narine Manukyan <narine.manukyan@technamin.com>
2022-02-08 23:03:08 +0400

    Initializing Services in Shopping Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions;
+
+public static class HttpClientExtensions
+{
+    public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+            throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+
+        var dataAsByteArray = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+
+        return JsonSerializer.Deserialize<T>(dataAsByteArray, new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+    }
+}
